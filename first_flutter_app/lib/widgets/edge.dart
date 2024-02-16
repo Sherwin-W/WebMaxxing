8e2950e7eb412461647b13cb2ac643513e78ef43
Ethan Dancho <ethan@dancho.org>
2024-02-16 03:34:38 -0700

    Added timer. No options for changing colors yet

--- first_flutter_app/lib/widgets/edge.dart
+++ first_flutter_app/lib/widgets/edge.dart
@@ -1,3 +1,5 @@
+import 'package:first_flutter_app/widgets/timer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EdgeWidget extends StatefulWidget {
@@ -10,31 +12,8 @@ class EdgeWidget extends StatefulWidget {
 class _EdgeWidgetState extends State<EdgeWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('Hours'),
-              Text('Minutes'),
-              Text('Seconds'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(onPressed: () {
-                // Implement start functionality
-              }, child: const Text('Start')),
-              ElevatedButton(onPressed: () {
-                // Implement pause functionality
-              }, child: const Text('Pause')),
-            ],
-          )
-        ],
-      ),
+    return const Center(
+      child: TimerScreen()
     );
   }
 }
