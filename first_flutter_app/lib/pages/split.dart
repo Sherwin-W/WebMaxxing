d1e5020319d6883d1e20a3e8f86b6a52cff5e745
Ethan Dancho <ethan@dancho.org>
2024-02-17 12:31:11 -0700

    Created bottomnavigation  bar that has more animations. The colors still need fine tuning which will be done when creating themes.

--- first_flutter_app/lib/pages/split.dart
+++ first_flutter_app/lib/pages/split.dart
@@ -1,8 +1,8 @@
+import 'package:first_flutter_app/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:first_flutter_app/widgets/quotes.dart';
 import 'package:first_flutter_app/widgets/edge.dart';
 import 'package:first_flutter_app/widgets/looks_maxxing.dart';
-import 'package:first_flutter_app/widgets/nav_item.dart';
 
 class SplitPage extends StatefulWidget {
   const SplitPage({super.key});
@@ -12,16 +12,9 @@ class SplitPage extends StatefulWidget {
 }
 
 class _SplitPageState extends State<SplitPage> {
-  int _selectedIndex = 0;
   final PageController _pageController = PageController();
+  int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      _pageController.animateToPage(index,
-          duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,34 +32,16 @@ class _SplitPageState extends State<SplitPage> {
           LooksMaxxingWidget(),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            NavItem(
-              icon: Icons.library_books,
-              label: 'Quote of The Day',
-              index: 0,
-              isSelected: _selectedIndex == 0,
-              onTap: _onItemTapped,
-            ),
-            NavItem(
-              icon: Icons.airline_seat_recline_extra,
-              label: 'Edging',
-              index: 1,
-              isSelected: _selectedIndex == 1,
-              onTap: _onItemTapped,
-            ),
-            NavItem(
-              icon: Icons.camera_alt,
-              label: 'LooksMaxxing',
-              index: 2,
-              isSelected: _selectedIndex == 2,
-              onTap: _onItemTapped,
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: SplitBottomBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: (index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 800),
+            curve: Curves.ease,
+          );
+        },
+      )
     );
   }
 }
